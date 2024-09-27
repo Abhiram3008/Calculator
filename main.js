249e98888370c81e86aded767626d13e56a76dac
Abhiram3008 <abhiram2430@gmail.com>
2024-09-27 11:26:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// display content
+function displayContent(content){
+    result.value += content
+}
+function calcScreenClear(){
+    result.value = ""
+}
+function calcOutput(){
+    result.value = eval(result.value)
+}
+function removeLastDigit(){
+    result.value = result.value.slice(0,-1)
+}
